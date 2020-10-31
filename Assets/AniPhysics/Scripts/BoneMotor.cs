0df3980665501f95e89e2007abe17c85547abe3e
Recstazy <djexstas9@gmail.com>
2020-10-31 14:34:43 +0700

    Added some shaders

---
+++ Assets/AniPhysics/Scripts/BoneMotor.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Recstazy.AniPhysics
+{
+    public class BoneMotor : MonoBehaviour
+    {
+        #region Fields
+
+        [SerializeField]
+        private Rigidbody connectedBody;
+
+        [SerializeField]
+        private bool useLocalPositions = false;
+
+        [SerializeField]
+        private StabSettings settings;
+
+        [SerializeField]
+        private float forceMagnitude = 1f;
+
+        private Vector3[] angularVelocities = new Vector3[3];
+
+        private float startDrag = 0f;
+        private float startAngularDrag = 0.05f;
+
+        #endregion
+
+        #region Properties
+
+        public bool IsGrabbing => CurrentBody != null;
+        public Rigidbody ConnectedBody => connectedBody;
+        public Rigidbody CurrentBody { get; private set; }
+        public StabSettings Settings
+        {
+            get => settings;
+            set
+            {
+                if (value != null)
+                {
+                    settings = value;
+                }
+            }
+        }
+
+        #endregion
+
+        private void FixedUpdate()
+        {
+            if (CurrentBody != connectedBody)
+            {
+                SetAttachedBody(connectedBody);
+            }
+
+            if (IsGrabbing)
+            {
+
+            }
+        }
+
+        public void SetAttachedBody(Rigidbody body)
+        {
+            if (body != null)
+            {
+                if (IsGrabbing)
+                {
+                    ReleaseBody();
+                }
+
+                AttachBody(body);
+            }
+            else
+            {
+                if (IsGrabbing)
+                {
+                    ReleaseBody();
+                }
+            }
+
+            connectedBody = CurrentBody;
+        }
+
+        private void ReleaseBody()
+        {
+            if (CurrentBody != null)
+            {
+                CurrentBody.drag = startDrag;
+                CurrentBody.angularDrag = startAngularDrag;
+
+                if (Settings.RotationStab)
+                {
+                    CurrentBody.AddTorque(GetAngularvelocity(), ForceMode.VelocityChange);
+                }
+
+                ChangeDetectionModeOnSleep(CurrentBody, CollisionDetectionMode.Discrete);
+                CurrentBody = null;
+            }
+        }
+
+        private void AttachBody(Rigidbody body)
+        {
+            CurrentBody = body;
+
+            if (CurrentBody != null)
+            {
+                startDrag = CurrentBody.drag;
+                startAngularDrag = CurrentBody.angularDrag;
+                CurrentBody.collisionDetectionMode = CollisionDetectionMode.ContinuousSpeculative;
+            }
+        }
+
+        private void ChangeDetectionModeOnSleep(Rigidbody body, CollisionDetectionMode mode)
+        {
+            StartCoroutine(ChangeDetectionModeRoutine(body, mode));
+        }
+
+        private IEnumerator ChangeDetectionModeRoutine(Rigidbody body, CollisionDetectionMode mode)
+        {
+            yield return new WaitUntil(() => body.IsSleeping());
+            body.collisionDetectionMode = mode;
+        }
+
+        private void WriteAngularVelocity(Vector3 velocity)
+        {
+            for (int i = 1; i < angularVelocities.Length; i++)
+            {
+                angularVelocities[i - 1] = angularVelocities[i];
+            }
+
+            angularVelocities[angularVelocities.Length - 1] = velocity;
+        }
+
+        private Vector3 GetAngularvelocity()
+        {
+            Vector3 result = default;
+
+            foreach (var v in angularVelocities)
+            {
+                result += v;
+            }
+
+            result /= angularVelocities.Length;
+
+            return result;
+        }
+    }
+}
---
+++ Assets/GrassShader/GeoGrass.shader
@@ -0,0 +1,81 @@
+/////////////////////////////////////////////////
+//////////////// GeoGrass.shader ////////////////
+/////////////////////////////////////////////////
+
+// @Cyanilux
+// Grass Geometry Shader, Written for Universal RP with help from https://roystan.net/articles/grass-shader.html
+
+Shader "Unlit/GeoGrass" {
+		Properties {
+			_Color("Colour", Color) = (1,1,1,1)
+			_Color2("Colour2", Color) = (1,1,1,1)
+			_Width("Width", Float) = 1
+			_RandomWidth("Random Width", Float) = 1
+			_Height("Height", Float) = 1
+			_RandomHeight("Random Height", Float) = 1
+			_WindStrength("Wind Strength", Float) = 0.1
+			[Space]
+			_TessellationUniform("Tessellation Uniform", Range(1, 64)) = 1
+		}
+
+		SubShader {
+			Tags { "RenderType" = "Opaque" "RenderPipeline" = "UniversalPipeline" }
+			LOD 300
+
+			Cull Off
+
+			Pass {
+				Name "ForwardLit"
+				Tags {"LightMode" = "UniversalForward"}
+
+				HLSLPROGRAM
+				// Required to compile gles 2.0 with standard srp library
+				#pragma prefer_hlslcc gles
+				#pragma exclude_renderers d3d11_9x gles
+				#pragma target 4.5
+
+				#pragma require geometry
+
+				#pragma vertex vert
+				#pragma geometry geom
+				#pragma fragment frag
+				#pragma hull hull
+				#pragma domain domain
+
+				#pragma multi_compile _ _MAIN_LIGHT_SHADOWS
+				#pragma multi_compile _ _MAIN_LIGHT_SHADOWS_CASCADE
+				#pragma multi_compile _ _SHADOWS_SOFT
+				#pragma require tessellation tessHW
+
+				// Defines
+
+				#define BLADE_SEGMENTS 1
+
+				// Includes
+
+				#include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Core.hlsl"
+				#include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Lighting.hlsl"
+				#include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Shadows.hlsl"
+				#include "GrassStructs.hlsl"
+				#include "Tessellation/CustomTessellation.hlsl"
+				#include "Grass.hlsl"
+
+				// Fragment
+
+				float4 frag(GeometryOutput input) : SV_Target {
+					#if SHADOWS_SCREEN
+						float4 clipPos = TransformWorldToHClip(input.positionWS);
+						float4 shadowCoord = ComputeScreenPos(clipPos);
+					#else
+						float4 shadowCoord = TransformWorldToShadowCoord(input.positionWS);
+					#endif
+
+					Light mainLight = GetMainLight(shadowCoord);
+
+					return lerp(_Color, _Color2, input.uv.y) * mainLight.shadowAttenuation;
+				}
+
+				ENDHLSL
+			}
+		}
+}
