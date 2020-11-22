a067c7ca125ff4987db83092a4ba948aa27df143
Recstazy <djexstas9@gmail.com>
2020-11-22 18:22:02 +0700

    Added gizmos, changed points parent to points list

--- Assets/AudioBeam/WaveBeamGen.cs
+++ Assets/AudioBeam/WaveBeamGen.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class WaveBeamGen : MonoBehaviour
@@ -15,7 +16,9 @@ public class WaveBeamGen : MonoBehaviour
     [SerializeField]
     private PointsMode pointsMode;
 
-    private Transform pointsParent;
+    [SerializeField]
+    private List<Transform> points;
+
     private GameObject rendererObject;
     private MeshFilter filter;
     private new MeshRenderer renderer;
@@ -36,7 +39,7 @@ public class WaveBeamGen : MonoBehaviour
 
     #region Properties
 
-    public List<Transform> Pivots { get; set; } = new List<Transform>();
+    public List<Transform> Points => points;
     public UVMode CurrentUVMode { get => uvMode; set => uvMode = value; }
     public PointsMode CurrentPointsMode { get => pointsMode; set => pointsMode = value; }
 
@@ -44,13 +47,13 @@ public class WaveBeamGen : MonoBehaviour
 
     private void Awake()
     {
-        CreatePivots();
+        //CreatePivots();
         CreateRenederer();
     }
 
     private void Update()
     {
-        if (pointsCount != pointsParent.childCount)
+        if (pointsCount != points.Count)
         {
             RegenerateAll();
         }
@@ -71,71 +74,76 @@ public class WaveBeamGen : MonoBehaviour
                 currentPointsMode = pointsMode;
             }
 
-            UpdateVertices(uvModeChanged, pointsModeChanged);
+            UpdateVertices();
+            UpdateUV(uvModeChanged, pointsModeChanged);
         }
     }
 
-    private void UpdateVertices(bool updateUV, bool updateUV1)
+    private void OnDrawGizmos()
+    {
+        if (points.Count > 1)
+        {
+            var lastColor = Gizmos.color;
+            Gizmos.color = new Color(1f, 0f, 0.5f, 0.5f);
+
+            for (int i = 0; i < points.Count - 1; i++)
+            {
+                var point = points[i];
+                var next = points[i + 1];
+
+                if (point != null && next != null)
+                {
+                    Gizmos.DrawLine(point.position, next.position);
+                }
+            }
+
+            Gizmos.color = lastColor;
+        }
+    }
+
+    private void UpdateVertices()
     {
         Transform startPoint;
+        Transform next;
         Vector3 directionToNext;
+        Vector3 right = transform.right * 0.1f;
 
         for (int i = 0; i < pointsCount - 1; i++)
         {
             int startI = i * 3;
 
-            startPoint = pointsParent.GetChild(i);
+            startPoint = points[i];
+            next = points[i + 1];
+
+            if (startPoint == null || next == null)
+            {
+                return;
+            }
+
             Vector3 startPosition = startPoint.localPosition;
 
-            directionToNext = pointsParent.GetChild(i + 1).localPosition - startPosition;
+            directionToNext = next.localPosition - startPosition;
 
-            vertices[startI] = startPosition - transform.right;
-            vertices[startI + 1] = startPosition + transform.right;
+            vertices[startI] = startPosition - right;
+            vertices[startI + 1] = startPosition + right;
             vertices[startI + 2] = startPosition + directionToNext;
-
-            if (updateUV)
-            {
-                if (uvMode == UVMode.PerSegment)
-                {
-                    FillUV(ref uv, i, startI, true);
-                }
-                else
-                {
-                    FillUV(ref uv, i, startI, true);
-                }
-            }
-
-            if (updateUV1)
-            {
-                if (pointsMode == PointsMode.Node)
-                {
-                    FillUV(ref uv1, i, startI, true);
-                }
-                else
-                {
-                    FillUV(ref uv1, i, startI, false);
-                }
-            }
         }
 
         filter.sharedMesh.vertices = vertices;
-
-        if (updateUV)
-        {
-            filter.sharedMesh.uv = uv;
-        }
-
-        if (updateUV1)
-        {
-            filter.sharedMesh.uv2 = uv1;
-        }
     }
 
     private void GenerateTriangles()
     {
-        for (int i = 0; i < triangles.Length; i++)
+        for (int i = 0; i < points.Count - 1; i++)
         {
-            triangles[i] = i;
+            if (points[i] == null)
+            {
+                continue;
+            }
+
+            triangles[i * 3] = i * 3;
+            triangles[i * 3 + 1] = i * 3 + 1;
+            triangles[i * 3 + 2] = i * 3 + 2;
         }
 
         filter.sharedMesh.triangles = triangles;
@@ -143,7 +151,7 @@ public class WaveBeamGen : MonoBehaviour
 
     private void RegenerateAll()
     {
-        pointsCount = pointsParent.childCount;
+        pointsCount = points.Count;
         enoughPoints = pointsCount > 1;
         filter.sharedMesh.Clear();
 
@@ -154,7 +162,8 @@ public class WaveBeamGen : MonoBehaviour
             uv = new Vector2[vertices.Length];
             uv1 = new Vector2[vertices.Length];
 
-            UpdateVertices(true, true);
+            UpdateVertices();
+            UpdateUV(true, true);
             GenerateTriangles();
         }
         else
@@ -185,7 +194,7 @@ public class WaveBeamGen : MonoBehaviour
     private void CreatePivots()
     {
         var pointsParentObject = new GameObject("PointsParent");
-        pointsParent = pointsParentObject.transform;
+        var pointsParent = pointsParentObject.transform;
         pointsParent.SetParent(transform);
         pointsParent.localPosition = Vector3.zero;
         pointsParent.localRotation = Quaternion.identity;
@@ -194,13 +203,55 @@ public class WaveBeamGen : MonoBehaviour
         pointA.transform.SetParent(pointsParent);
         pointA.transform.localPosition = Vector3.zero;
         pointA.transform.localRotation = Quaternion.identity;
-        Pivots.Add(pointA.transform);
+        Points.Add(pointA.transform);
 
         var pointB = new GameObject("PointB");
         pointB.transform.SetParent(pointsParent);
         pointB.transform.localPosition = Vector3.up;
         pointB.transform.localRotation = Quaternion.identity;
-        Pivots.Add(pointB.transform);
+        Points.Add(pointB.transform);
+    }
+
+    private void UpdateUV(bool updateUV, bool updateUV1)
+    {
+        for (int i = 0; i < pointsCount - 1; i++)
+        {
+            int startI = i * 3;
+
+            if (updateUV)
+            {
+                if (uvMode == UVMode.PerSegment)
+                {
+                    FillUV(ref uv, i, startI, true);
+                }
+                else
+                {
+                    FillUV(ref uv, i, startI, true);
+                }
+            }
+
+            if (updateUV1)
+            {
+                if (pointsMode == PointsMode.Node)
+                {
+                    FillUV(ref uv1, i, startI, true);
+                }
+                else
+                {
+                    FillUV(ref uv1, i, startI, false);
+                }
+            }
+        }
+
+        if (updateUV)
+        {
+            filter.sharedMesh.uv = uv;
+        }
+
+        if (updateUV1)
+        {
+            filter.sharedMesh.uv2 = uv1;
+        }
     }
 
     private void FillUV(ref Vector2[] uv, int i, int startI, bool perSegment)
